9ec61d2d6cd4a700ad36a5ae11d669cc58dd8b0b
Hati0kooHati <dserion.prn@gmail.com>
2025-11-18 11:31:16 +0600

    add env

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,5 +3,6 @@ class Constants {
 
   static const pixabayApiKey = String.fromEnvironment("PIXABAY_API_KEY");
   static const token = String.fromEnvironment("TOKEN");
+  static const env = String.fromEnvironment("ENV");
   static const pixabayBaseUrl = "https://pixabay.com/api/";
 }
